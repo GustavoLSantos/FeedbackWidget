5274dac3d62de28e0c1678bad7efde501056e816
Gustavo Lira <gustavolirasn@gmail.com>
2022-05-05 17:28:51 -0300

    Project Done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import DarkModeIcon from '@mui/icons-material/DarkMode';
+import WbSunnyIcon from '@mui/icons-material/WbSunny';
+import Box from "./components/Box";
+import {useState} from "react";
 
 function App() {
+
+  const [colorType, setColorType] = useState(true)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={colorType === true ? "pagina" : "paginaLight"}> 
+    <div className= "font" onClick={() => setColorType(!colorType)}> 
+    {colorType === true ? 
+    <p className='darkTitle'>Dark Mode <DarkModeIcon className='moonsize' fontSize='5000px'/></p>
+    :
+    <p className='lightTitle'>Light Mode <WbSunnyIcon className='moonsize' fontSize='5000px'/></p>
+  }
+    </div>
+    <div className="linha"/>
+
+    <div>
+    <div className='boxRow'>
+      <Box tipo={"problema"} colorMode={colorType}/>
+      <Box tipo={"ideia"} colorMode={colorType}/>
+      <Box tipo={"outro"} colorMode={colorType}/>
+    </div>
+
+
+  
+
+      
+    </div>
     </div>
   );
 }
